1f9ecf281b504ddfb5f107d38ce376fe50f5c099
Masadi Akash <113844493+akash-masadi@users.noreply.github.com>
2023-07-02 16:06:58 +0530

    Connecting to DB

--- models/user.model.js
+++ models/user.model.js
@@ -11,9 +11,32 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
     },
+
     userID: { 
     type: String,
     required: true,
     unique: true
+    },
+
+    password: {
+    type: String,
+    required : true
+    // minLength:8
+    },
+
+    email: {
+    type : String,
+    required : true,
+    unique : true,
+    minLength : 10,
+    lowercase : true
+    },
+
+    userType:
+    {
+        type: String,
+        require : true,
+        default : "CUSTOMER",
+        enum : ["CUSTOMER","ADMIN"]
     }
-});
+},{timestamps:true});
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express =  require('express');
 const serverConfig = require('./config/server.config');
 const app = express();
+const mongoose =require('mongoose');
+const dbConfig=require('./config/db.config');
+
+/*
+*Logic to connect the MongoDB and create an Admin user
+*/
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+db.on('error',()=>{
+    console.error.bind(console,'connection error:')}
+    );
+db.once('open',()=>{
+    console.log("Connection Successful!");
+});
+
+
 app.listen(serverConfig.port, () => {
     console.log('Server running on port 3333');
 });
